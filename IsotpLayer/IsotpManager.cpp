e33579f821708966e631916f6fe670213e1893c2
Mirko Braic <mirko.braic@gmail.com>
2020-04-29 11:27:11 +0200

    Small bug fix

--- IsotpLayer/IsotpManager.cpp
+++ IsotpLayer/IsotpManager.cpp
@@ -9,15 +9,8 @@ IsotpManager::IsotpManager(CanBusProvider provider, std::vector<std::pair<uint32
 {
     // QUESTION: what exacly are sender and recipient id and how do they relate to can message id
     isotpTransport.set_received_message_callback([this] (std::shared_ptr<uds::uds_message<uint32_t>> message) {
-//        try {
-//            CanMessage canMessage = CanMessage(message->recipient_id, message->data.size(), message->data);
-//            emit newMessageRecieved(canMessage);
-//        } catch (std::exception e) {
-//            qDebug() << "CanMessage constructor error:  " << e.what();
-//        }
-
         uds::uds_message<uint32_t> msg;
-        msg.data = std::vector<uint8_t>() = {0x5, 0x41, 0x5, 0x4, 0x3, 0x0};
+        msg.data = std::vector<uint8_t>() = {0x41, 0x5, 0x4, 0x3, 0x0};
         msg.sender_id = 2;
         msg.recipient_id = 1;
         isotpTransport.send_message_to_peer(std::make_unique<uds::uds_message<uint32_t>>(msg));
